378b486f473ce57b72d0088ec7d215be5080a4cb
1299088269 <1299088269@qq.com>
2020-01-13 23:40:59 +0800

    程序随机生成100个0-99之间的随机整数存入数组，选取一种合适的流，将该数组的100个整数写入本地文件。

---
+++ src/com/soft1851/study/week1/RandomTest.java
@@ -0,0 +1,43 @@
+package com.soft1851.study.week1;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Random;
+
+/**
+ * @ClassName RandomTest
+ * @Description TODO
+ * @Author 田震
+ * @Date 2020/1/13
+ **/
+public class RandomTest {
+    public static void main(String[] args) {
+        final int number = 100;
+        Random random = new Random();
+        String[] text = new String[100];
+        int content;
+        for (int i = 0;i<number;i++){
+            content = random.nextInt(number);
+            text[i] = content+"\n";
+        }
+        File file = new File("D:/java_test/randomText.txt");
+        try {
+            if (!file.exists()){
+                boolean exist = file.createNewFile();
+                if (exist){
+                    System.out.println("自动创建该文件！");
+                }
+            }
+            FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for (String s : text) {
+                bufferedWriter.write(s);
+            }
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (Exception e){
+            System.out.println("写入失败");
+        }
+    }
+}
